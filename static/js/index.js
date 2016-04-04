f75c408e58c3346c7247e20e422fd58cc80252ec
Josh Shifman <shifmanj@umich.edu>
2016-04-03 20:10:10 -0400

    add typing effect

---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".typed").typed({
+    	// console.Log("FUNCTION WORKING")
+        strings: ["Hi I'm Josh.", "It's nice to meet you."],
+        typeSpeed: 80
+    });
+});
